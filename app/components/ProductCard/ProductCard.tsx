911fe7750ef1776ed130560d3c88d89f8f4d65ae
Felipe Cancino <jf.cancino@conceptcshop.com>
2026-03-20 07:12:37 -0400

    temporally remove bin validation function

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -49,6 +49,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
       setTimeout(() => {
         setBinValidated(false);
         setBinLocationValue("");
+        console.log(binValidated);
       }, 0);
     }
   }, [isAnyModalOpen]);
@@ -325,7 +326,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
             <p>Voulez-vous confirmer la mise à jour du produit&nbsp;?</p>
             <p style={{ marginTop: "8px" }}><strong>{product.title}</strong></p>
             <span className="modal-sku">SKU: {activeVariant?.sku || "N/A"}</span>
-            <div className="input-group bin-validation-group mt-4!" style={{ width: "220px" }}>
+            <div className="input-group bin-validation-group mt-4! hidden!" style={{ width: "220px" }}>
               <span className="input-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
@@ -342,7 +343,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
             </div>
             {
               (Array.isArray(product.bin_location) ? product.bin_location : product.bin_location?.split(',') || []).map((location:string, index: number) => (
-                <span key={index} className={`modal-sku mr-2! ${binLocationValue === location.trim() && 'bg-green-100! border-green-700!'} `}>{location.trim()}</span>
+                <span key={index} className={`modal-sku mr-2! ${binLocationValue === location.trim() && 'bg-green-100! border-green-700! '} hidden!`}>{location.trim()}</span>
               ))
             }
           </div>
@@ -352,7 +353,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
         onConfirm={handleConfirm}
         onClose={() => setShowModal(false)}
         inline
-        disabledConfirmButton={!binValidated}
+        // disabledConfirmButton={!binValidated}
       />
 
       <Modal
@@ -366,7 +367,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
             <p style={{ marginTop: "10px" }}>
               La quantité sera mise à <strong>{product.bin_max_quantity}</strong> (max bin).
             </p>
-            <div className="input-group bin-validation-group mt-4!" style={{ width: "220px" }}>
+            <div className="input-group bin-validation-group mt-4! hidden!" style={{ width: "220px" }}>
               <span className="input-icon">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" d="m20.25 7.5-.625 10.632a2.25 2.25 0 0 1-2.247 2.118H6.622a2.25 2.25 0 0 1-2.247-2.118L3.75 7.5M10 11.25h4M3.375 7.5h17.25c.621 0 1.125-.504 1.125-1.125v-1.5c0-.621-.504-1.125-1.125-1.125H3.375c-.621 0-1.125.504-1.125 1.125v1.5c0 .621.504 1.125 1.125 1.125Z" />
@@ -383,7 +384,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
             </div>
             {
               (Array.isArray(product.bin_location) ? product.bin_location : product.bin_location?.split(',') || []).map((location:string, index: number) => (
-                <span key={index} className={`modal-sku mr-2! ${binLocationValue === location.trim() && 'bg-green-100! border-green-700!'} `}>{location.trim()}</span>
+                <span key={index} className={`modal-sku mr-2! ${binLocationValue === location.trim() && 'bg-green-100! border-green-700!'} hidden!`}>{location.trim()}</span>
               ))
             }
           </div>
@@ -393,7 +394,7 @@ export default function ProductCard({ product, onConfirm, onDelete, foundedCardK
         onConfirm={handleFillBinConfirm}
         onClose={() => setShowFillModal(false)}
         inline
-        disabledConfirmButton={!binValidated}
+        // disabledConfirmButton={!binValidated}
       />
 
       <Modal
