68a8907750f763b4fbcf5dc55168ac490ad9279b
Felipe Cancino <jf.cancino@conceptcshop.com>
2026-07-10 14:43:01 -0400

    add loading states

--- app/components/MapList/MapList.tsx
+++ app/components/MapList/MapList.tsx
@@ -5,10 +5,12 @@ import "../MapBin/MapBin.css";
 import { Button } from "@/components/ui/button";
 import { BinLocationsSection100, BinLocationsSection200, BinLocationsSection300, BinLocationsSection400, BinLocationSection500, BinLocationSection600, BinLocationSection700 } from "@/app/lib/data/warehouse_bin_locations";
 import { updateBinLocations } from "@/app/lib/data/updateBinLocations";
+import Loading from "../Loading/Loading";
 
 export default function MapList() {
 
   const [binLocations, setBinLocations] = useState<BinGroup[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   const sections = [
     { id: 'section-100', name: 'Section 100', initialNumber: '1' },
@@ -35,6 +37,8 @@ export default function MapList() {
     catch (error) {
       console.error('Error fetching bin locations:', error);
       return [];
+    } finally {
+      setLoading(false);
     }
   }
 
@@ -111,112 +115,115 @@ export default function MapList() {
 
   return (
     <div className="map-list">
-      <Tabs orientation="vertical" defaultValue="section-100">
-        <TabsList className="w-[150px]">
-          {sections.map((section: { id: string; name: string; initialNumber: string }) => (
-            <TabsTrigger
-              key={section.id}
-              value={section.id}
-              className="p-4! text-center cursor-pointer"
-            >
-              {section.name}
+      {
+        loading ? <Loading /> : 
+        <Tabs orientation="vertical" defaultValue="section-100">
+          <TabsList className="w-[150px]">
+            {sections.map((section: { id: string; name: string; initialNumber: string }) => (
+              <TabsTrigger
+                key={section.id}
+                value={section.id}
+                className="p-4! text-center cursor-pointer"
+              >
+                {section.name}
+              </TabsTrigger>
+            ))}
+            <TabsTrigger disabled className="p-4! text-center cursor-pointer" value="import-locations">
+              Import locations
             </TabsTrigger>
+          </TabsList>
+          {sections.map((section: { id: string; name: string; initialNumber: string }) => (
+            <TabsContent key={section.id} value={section.id}>
+              <div className="map-section">
+                <div className="map-placeholder">
+                  <ul className="grid grid-cols-8 gap-4">
+                    {
+                      binLocations
+                        .filter((location: BinGroup) => location.id.startsWith(section.initialNumber))
+                        .map((location: BinGroup) => (
+                          <li
+                            key={location.id}
+                            className="relative overflow-hidden">
+                            <div className="bin-card">
+                              <details className="sub-bins rounded-lg overflow-hidden border-2 border-neutral-300">
+                                <summary className="relative
+                                    sub-bin--header
+                                    px-4!
+                                    py-2!
+                                    bg-neutral-200
+                                    text-neutral-600
+                                    border-neutral-200
+                                    text-lg
+                                    font-bold
+                                    cursor-pointer">
+                                  <span>{location.id}</span>
+                                  <span className="block text-sm font-normal">
+                                    {location.sku}
+                                  </span>
+                                </summary>
+                                <div className="sub-bin--body bg-neutral-200">
+                                  {
+                                    location.bins.length > 0 ? (
+                                      location.bins.map((bin: BinItem) => (
+                                        <div key={bin.id} className="px-9! py-2! flex justify-between">
+                                          <span>{bin.id}</span>
+                                          <span className="text-sm text-neutral-500">
+                                            {bin.sku}
+                                          </span>
+                                        </div>
+                                      ))
+                                    ) : (
+                                      <p className="px-9! py-2! text-sm text-neutral-400">
+                                        Not sub-bins
+                                      </p>
+                                    )
+                                  }
+                                </div>
+                              </details>
+                            </div>
+                          </li>
+                        ))
+                    }
+                  </ul>
+                </div>
+              </div>
+            </TabsContent>
           ))}
-          <TabsTrigger disabled className="p-4! text-center cursor-pointer" value="import-locations">
-            Import locations
-          </TabsTrigger>
-        </TabsList>
-        {sections.map((section: { id: string; name: string; initialNumber: string }) => (
-          <TabsContent key={section.id} value={section.id}>
+          <TabsContent value="import-locations">
             <div className="map-section">
               <div className="map-placeholder">
-                <ul className="grid grid-cols-8 gap-4">
-                  {
-                    binLocations
-                      .filter((location: BinGroup) => location.id.startsWith(section.initialNumber))
-                      .map((location: BinGroup) => (
-                        <li
-                          key={location.id}
-                          className="relative overflow-hidden">
-                          <div className="bin-card">
-                            <details className="sub-bins rounded-lg overflow-hidden border-2 border-neutral-300">
-                              <summary className="relative
-                                  sub-bin--header
-                                  px-4!
-                                  py-2!
-                                  bg-neutral-200
-                                  text-neutral-600
-                                  border-neutral-200
-                                  text-lg
-                                  font-bold
-                                  cursor-pointer">
-                                <span>{location.id}</span>
-                                <span className="block text-sm font-normal">
-                                  {location.sku}
-                                </span>
-                              </summary>
-                              <div className="sub-bin--body bg-neutral-200">
-                                {
-                                  location.bins.length > 0 ? (
-                                    location.bins.map((bin: BinItem) => (
-                                      <div key={bin.id} className="px-9! py-2! flex justify-between">
-                                        <span>{bin.id}</span>
-                                        <span className="text-sm text-neutral-500">
-                                          {bin.sku}
-                                        </span>
-                                      </div>
-                                    ))
-                                  ) : (
-                                    <p className="px-9! py-2! text-sm text-neutral-400">
-                                      Not sub-bins
-                                    </p>
-                                  )
-                                }
-                              </div>
-                            </details>
-                          </div>
-                        </li>
-                      ))
-                  }
-                </ul>
+                <Button className="px-4! py-2!" onClick={async () => updateBinLocations(await BinLocationsSection100())}>
+                  Import Bin locations for section 100
+                </Button>
+
+                <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationsSection200())}>
+                  Import Bin locations for section 200
+                </Button>
+
+                <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationsSection300())}>
+                  Import Bin locations for section 300
+                </Button>
+
+                <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationsSection400())}>
+                  Import Bin locations for section 400
+                </Button>
+
+                <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationSection500())}>
+                  Import Bin locations for section 500
+                </Button>
+
+                <Button className="px-4! py-2! mt-4!" onClick={async () => updateBinLocations(await BinLocationSection600())}>
+                  Import Bin locations for section 600
+                </Button>
+
+                <Button className="px-4! py-2! mt-4! ml-4!" onClick={async () => updateBinLocations(await BinLocationSection700())}>
+                  Import Bin locations for section 700
+                </Button>
               </div>
             </div>
           </TabsContent>
-        ))}
-        <TabsContent value="import-locations">
-          <div className="map-section">
-            <div className="map-placeholder">
-              <Button className="px-4! py-2!" onClick={async () => updateBinLocations(await BinLocationsSection100())}>
-                Import Bin locations for section 100
-              </Button>
-
-              <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationsSection200())}>
-                Import Bin locations for section 200
-              </Button>
-
-              <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationsSection300())}>
-                Import Bin locations for section 300
-              </Button>
-
-              <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationsSection400())}>
-                Import Bin locations for section 400
-              </Button>
-
-              <Button className="px-4! py-2! ml-4!" onClick={async () => updateBinLocations(await BinLocationSection500())}>
-                Import Bin locations for section 500
-              </Button>
-
-              <Button className="px-4! py-2! mt-4!" onClick={async () => updateBinLocations(await BinLocationSection600())}>
-                Import Bin locations for section 600
-              </Button>
-
-              <Button className="px-4! py-2! mt-4! ml-4!" onClick={async () => updateBinLocations(await BinLocationSection700())}>
-                Import Bin locations for section 700
-              </Button>
-            </div>
-          </div>
-        </TabsContent>
-      </Tabs>
+        </Tabs>
+      }
     </div>
   )
 }
